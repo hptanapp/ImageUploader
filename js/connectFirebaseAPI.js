76cff8bff5d4095da75724e435dbaac40a8456bd
hptanapp <hptanappads@gmail.com>
2017-12-31 00:40:28 +0800

    added form function

---
+++ js/connectFirebaseAPI.js
@@ -0,0 +1,16 @@
+// Initialize Firebase & Connect FireBase API
+var config = {
+  apiKey: "AIzaSyBvEgDexqUNE0nO_ZE9zJhcb3zSDrm_8Nw",
+  authDomain: "imageuploader-3b27c.firebaseapp.com",
+  databaseURL: "https://imageuploader-3b27c.firebaseio.com",
+  projectId: "imageuploader-3b27c",
+  storageBucket: "imageuploader-3b27c.appspot.com",
+  messagingSenderId: "285832792911"
+};
+firebase.initializeApp(config);
+
+// Get a reference to the storage service, which is used to create references in your storage bucket
+var storage = firebase.storage();
+
+// Get a reference to the database service
+var database = firebase.database();
---
+++ js/imageHandler.js
@@ -0,0 +1,59 @@
+// For image handling & functions
+
+function UploadFile() {
+  input = document.getElementById('profile_pic');
+  thisFile = input.files[0];
+  thisFileName = thisFile.name;
+
+  var file = thisFile // use the Blob or File API
+
+  // Create a root reference
+  var storageRef = firebase.storage().ref();
+  
+  // Upload the file and metadata
+  //var uploadTask = storageRef.child('download.jpg').put(file, metadata);
+  var uploadTask = storageRef.child(thisFileName).put(file);
+  
+  // ref.put(file).then(function(snapshot) {
+  //   console.log('Uploaded a blob or file!');
+  // });
+
+  // Register three observers:
+  // 1. 'state_changed' observer, called any time the state changes
+  // 2. Error observer, called on failure
+  // 3. Completion observer, called on successful completion
+  uploadTask.on('state_changed', function(snapshot){
+    // Observe state change events such as progress, pause, and resume
+    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+    var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    console.log('Upload is ' + progress + '% done');
+    switch (snapshot.state) {
+      case firebase.storage.TaskState.PAUSED: // or 'paused'
+        console.log('Upload is paused');
+        break;
+      case firebase.storage.TaskState.RUNNING: // or 'running'
+        console.log('Upload is running');
+        break;
+    }
+  }, function(error) {
+    // Handle unsuccessful uploads
+    alert("Upload Error");
+  }, function() {
+    // Handle successful uploads on complete
+    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+    var downloadURL = uploadTask.snapshot.downloadURL;
+  });
+}
+
+function showimage() {
+   var storageRef = firebase.storage().ref();
+   var Filter = document.getElementById ("filter");
+   var filterString = Filter.value;
+   storageRef.child(filterString).getDownloadURL().then(function(url) {
+       var test = url;
+       document.querySelector('img').src = test;
+       var loading = document.getElementById ("showimage");
+       loading.style.visibility = "visible";
+   }).catch(function(error) {
+   });
+ }
