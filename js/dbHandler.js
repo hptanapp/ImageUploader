0edc466da2ce89e0a4dc08254da38ff549137a2a
hptanapp <hptanappads@gmail.com>
2018-01-02 03:05:26 +0800

    Added function to db

---
+++ js/dbHandler.js
@@ -0,0 +1,63 @@
+function Upload(){
+  if (userLogon){
+
+  }
+  else{
+    GetUniqueGeo();
+  }
+}
+
+function GetUniqueGeo(){
+  $.getJSON('https://www.freegeoip.net/json/?callback=?', function(data) {
+    console.log(JSON.stringify(data, null, 2));
+    retString = JSON.stringify(data, null, 2);
+    var list_geoInfo = retString.split(',');
+    for (i = 0; i < list_geoInfo.length; i++) {
+      if ( list_geoInfo[i].includes("latitude")){
+        mylatitude = list_geoInfo[i];
+        mylatitude = mylatitude.split(':')[1].trim();
+      }
+      if ( list_geoInfo[i].includes("longitude")){
+        mylongitude = list_geoInfo[i];
+        mylongitude = mylongitude.split(':')[1].trim();
+      }
+    }
+
+    uniqueUser = mylatitude + "_" + mylongitude;
+    uniqueUser = uniqueUser.replace(/\./g,":");
+    uniqueUserTime = uniqueUser + "_" + Submitter_Date;
+
+    console.log(uniqueUser);
+    console.log(uniqueUserTime);
+    writeToDb();
+  });
+}
+
+function writeToDb(){
+  firebase.database().ref('users/' + Submitter_Name + "_" + uniqueUserTime ).set({
+    type: "dbtype",
+    Submitter_Name: Submitter_Name,
+    Submitter_Email: Submitter_Email,
+    Submitter_Date: Submitter_Date,
+    Submitter_Type: Submitter_Type,
+    Name: Name,
+    CompanyName: CompanyName,
+    ServiceField: ServiceField,
+    Position: Position,
+    Contact: Contact,
+    Address: Address,
+    WorkingHour: WorkingHour,
+    CoverageArea: CoverageArea,
+    AdditionalTags: AdditionalTags,
+    IdToImageFront: "-",
+    IdToImageBack: "-"
+  });
+}
+
+function readFromDb(){
+  var userId = firebase.auth().currentUser.uid;
+  return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
+    var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+    // ...
+  });
+}
--- js/submitForms.js
+++ js/submitForms.js
@@ -1,39 +1,170 @@
 function prepareUploadSubForm(){
-    for (i = 0; i < list_subkey.length; i++) { 
-      var mykey = list_subkey[i];
-      var label = document.createElement("label");
-      label.setAttribute('for', mykey);
-      label.innerHTML = mykey + ":";
-      var input = document.createElement("input");
-      input.setAttribute('type', 'text');
-      input.setAttribute('id', mykey);
-      var br = document.createElement("br");
-      
-      var parent = document.getElementById("SubmitterInfoPlaceholder");
-      parent.appendChild(label);
-      parent.appendChild(input);
-      parent.appendChild(br);
-    }
-    var thisformbutton = document.getElementById("btn_prepareUploadSubForm");
-    thisformbutton.style.visibility = "hidden";
+  for (i = 0; i < list_subkey.length; i++) { 
+    var mykey = list_subkey[i];
+    var label = document.createElement("label");
+    label.setAttribute('for', mykey);
+    label.innerHTML = mykey + ":";
+    var input = document.createElement("input");
+    input.setAttribute('type', 'text');
+    input.setAttribute('id', mykey);
+    var br = document.createElement("br");
+    
+    var parent = document.getElementById("SubmitterInfoPlaceholder");
+    parent.appendChild(label);
+    parent.appendChild(input);
+    parent.appendChild(br);
+  }
+  var thisformbutton = document.getElementById("btn_prepareUploadSubForm");
+  thisformbutton.style.visibility = "hidden";
+}
+
+function prepareUploadCardForm(){
+  for (i = 0; i < list_cardkey.length; i++) { 
+    var mykey = list_cardkey[i];
+    var label = document.createElement("label");
+    label.setAttribute('for', mykey);
+    label.innerHTML = mykey + ":";
+    var input = document.createElement("input");
+    input.setAttribute('type', 'text');
+    input.setAttribute('id', mykey);
+    var br = document.createElement("br");
+    
+    var parent = document.getElementById("CardInfoPlaceholder");
+    parent.appendChild(label);
+    parent.appendChild(input);
+    parent.appendChild(br);
+  }
+  var thisformbutton = document.getElementById("btn_prepareUploadCardForm");
+  thisformbutton.style.visibility = "hidden";
+}
+
+// UTC time
+function ISODateString(d) {
+  function pad(n) {return n<10 ? '0'+n : n}
+  return d.getUTCFullYear()+'-'
+       + pad(d.getUTCMonth()+1)+'-'
+       + pad(d.getUTCDate())+'T'
+       + pad(d.getUTCHours())+':'
+       + pad(d.getUTCMinutes())+':'
+       + pad(d.getUTCSeconds())+'Z'
+}
+
+//Get Form info
+function writeUserData() {
+  // Submitter_Name
+  var intextbox = document.getElementById("Submitter_Name");
+  if (intextbox.value != ""){
+    Submitter_Name = intextbox.value;
+  } else {
+    Submitter_Name = "guest";
   }
 
-  function prepareUploadCardForm(){
-    for (i = 0; i < list_cardkey.length; i++) { 
-      var mykey = list_cardkey[i];
-      var label = document.createElement("label");
-      label.setAttribute('for', mykey);
-      label.innerHTML = mykey + ":";
-      var input = document.createElement("input");
-      input.setAttribute('type', 'text');
-      input.setAttribute('id', mykey);
-      var br = document.createElement("br");
-      
-      var parent = document.getElementById("CardInfoPlaceholder");
-      parent.appendChild(label);
-      parent.appendChild(input);
-      parent.appendChild(br);
-    }
-    var thisformbutton = document.getElementById("btn_prepareUploadCardForm");
-    thisformbutton.style.visibility = "hidden";
+  // Submitter_Email
+  var intextbox = document.getElementById("Submitter_Email");
+  if (intextbox.value != ""){
+    Submitter_Email = intextbox.value;
+  } else {
+    Submitter_Email = "-";
   }
+
+  // Submitter_Date
+  var d = new Date();
+  Submitter_Date = ISODateString(d);
+
+  // Submitter_Email
+  var intextbox = document.getElementById("Submitter_Type");
+  if (intextbox.value != ""){
+    Submitter_Type = intextbox.value;
+  } else {
+    Submitter_Type = "public";
+  }
+
+  // Name
+  var intextbox = document.getElementById("Name");
+  if (intextbox.value != ""){
+    Name = intextbox.value;
+  } else {
+    Name = "-";
+  }
+
+  // CompanyName
+  var intextbox = document.getElementById("CompanyName");
+  if (intextbox.value != ""){
+    CompanyName = intextbox.value;
+  } else {
+    CompanyName = "-";
+  }
+
+  // ServiceField
+  var intextbox = document.getElementById("ServiceField");
+  if (intextbox.value != ""){
+    ServiceField = intextbox.value;
+  } else {
+    ServiceField = "-";
+  }
+
+  // Position
+  var intextbox = document.getElementById("Position");
+  if (intextbox.value != ""){
+    Position = intextbox.value;
+  } else {
+    Position = "-";
+  }
+
+  // Contact
+  var intextbox = document.getElementById("Contact");
+  if (intextbox.value != ""){
+    Contact = intextbox.value;
+  } else {
+    Contact = "-";
+  }
+
+  // Address
+  var intextbox = document.getElementById("Address");
+  if (intextbox.value != ""){
+    Address = intextbox.value;
+  } else {
+    Address = "-";
+  }
+
+  // WorkingHour
+  var intextbox = document.getElementById("WorkingHour");
+  if (intextbox.value != ""){
+    WorkingHour = intextbox.value;
+  } else {
+    WorkingHour = "-";
+  }
+
+  // CoverageArea
+  var intextbox = document.getElementById("CoverageArea");
+  if (intextbox.value != ""){
+    CoverageArea = intextbox.value;
+  } else {
+    CoverageArea = "-";
+  }
+
+  // AdditionalTags
+  var intextbox = document.getElementById("AdditionalTags");
+  if (intextbox.value != ""){
+    AdditionalTags = intextbox.value;
+  } else {
+    AdditionalTags = "-";
+  }
+
+  Upload();
+  // console.log(Submitter_Name);
+  // console.log( Submitter_Email);
+  // console.log( Submitter_Date);
+  // console.log( Submitter_Type);
+
+  // console.log( Name);
+  // console.log( CompanyName);
+  // console.log( ServiceField);
+  // console.log( Position);
+  // console.log( Contact);
+  // console.log( Address);
+  // console.log( WorkingHour);
+  // console.log( CoverageArea);
+  // console.log( AdditionalTags);
+
+}
